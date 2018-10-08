8784ea2c65c6075ab5386979360be58ed89c7984
THEGREENMACHINE\dkamp <jd856236@wcupa.edu>
2018-10-08 16:56:35 -0400

    Adding labyrinth project

---
+++ Labyrinth/Assets/Scripts/BallController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");                     //get the input from the user
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);     //move based on the input
+        rb.AddForce(movement * speed);
+	}
+}
